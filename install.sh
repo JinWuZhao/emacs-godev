e1ae9904093bcf3b4a5c33fff1617634962c6168
jin.zhao <jin.zhao@xinda.im>
2018-05-18 00:19:19 +0800

    change plugin oracle to gure

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ export GOPATH=`pwd`
 
 go get -u -v github.com/nsf/gocode
 go get -u -v github.com/rogpeppe/godef
-go get -u -v golang.org/x/tools/cmd/oracle
+go get -u -v golang.org/x/tools/cmd/guru
 go get -u -v golang.org/x/tools/cmd/gorename
 go get -u -v golang.org/x/tools/cmd/goimports
 go get -u -v github.com/alecthomas/gometalinter
