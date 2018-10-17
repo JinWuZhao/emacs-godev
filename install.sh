1cbab98595a024183f7ab50db56d28d349cbc892
jin <jinzhao.info@gmail.com>
2018-10-17 15:33:41 +0800

    change base to archlinux

--- install.sh
+++ install.sh
@@ -1,29 +1,11 @@
 #!/bin/sh
 
-apk --no-cache add go
-apk --no-cache add mysql-client
+pacman -S --noconfirm go
+pacman -S --noconfirm mysql-client
 
-if [ ! -e /etc/nsswitch.conf ]
-then
-    echo 'hosts: files dns' > /etc/nsswitch.conf
-fi
-
-cd /mnt
-
-export GOROOT_BOOTSTRAP="$(go env GOROOT)"
-export GOOS="$(go env GOOS)"
-export GOARCH="$(go env GOARCH)"
-export GOHOSTOS="$(go env GOHOSTOS)"
-export GOHOSTARCH="$(go env GOHOSTARCH)"
-wget -O go.tar.gz https://dl.google.com/go/go1.11.1.src.tar.gz
-tar -C /usr/local -xzf go.tar.gz
-rm go.tar.gz
-cd /usr/local/go/src
-./make.bash
-apk del go
-ln -s /usr/local/go/bin/* /usr/local/bin/
 go version
 
+cd /mnt
 mkdir /mnt/go
 cd /mnt/go
 export GOPATH=`pwd`
